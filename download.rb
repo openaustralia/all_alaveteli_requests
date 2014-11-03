8cf3c2a83afccba57eacf4b9f2ef3ff04cdc2eb9
Matthew Landauer <matthew@oaf.org.au>
2014-11-03 16:17:47 +1100

    Download all the zips

--- download.rb
+++ download.rb
@@ -7,6 +7,18 @@
 # we're going to scrape the pages. Yup.
 
 require "mechanize"
+require 'json'
+
+def load_secrets
+  if File.exists?(".secrets")
+    YAML.load_file(".secrets")
+  else
+    puts "Create a file .secrets and put your Alaveteli email and password there like this:"
+    puts 'email: YOUR@EMAIL'
+    puts 'password: YOURPASSWORD'
+    exit
+  end
+end
 
 def all_request_urls
   agent = Mechanize.new
@@ -33,4 +45,40 @@ def all_request_urls
   links
 end
 
-p all_request_urls
+#requests = ["https://www.righttoknow.org.au/request/the_socio_economic_impact_assess"]
+requests = all_request_urls
+secrets = load_secrets
+
+
+agent = Mechanize.new
+page = agent.get("https://www.righttoknow.org.au/profile/sign_in")
+form = page.form_with(id: "signin_form")
+email_field = form.field_with(name: "user_signin[email]")
+password_field = form.field_with(name: "user_signin[password]")
+email_field.value = secrets["email"]
+password_field.value = secrets["password"]
+page = form.submit
+
+# First need to login because we can't download the zip file without being logged in
+
+requests.each do |request|
+  json = agent.get("#{request}.json").body.to_s
+  page = JSON.parse(json)
+  id = page["id"]
+  if File.exists?("data/#{id}")
+    puts "Skipping request #{id}. Already downloaded."
+  else
+    puts "Downloading data for request #{id}..."
+    begin
+      # Create a directory based on the id
+      FileUtils.mkdir_p("data/#{id}")
+      File.open("data/#{id}/request.json", "w") {|f| f.write(json)}
+      zip = agent.get("#{request}/download").body
+      File.open("data/#{id}/download.zip", "w") {|f| f.write(zip)}
+    rescue
+      # Any problems just delete the whole directory
+      FileUtils.rm_rf("data/#{id}")
+      raise
+    end
+  end
+end
